c95e2b1c82955c494c6f03cc8c90b8ac48ff501f
Saad Ul Hassan <saad.ul.hussan17@gmail.com>
2023-07-05 03:15:49 +0500

    Initial UI setup in App component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { Typography, Tabs } from "antd";
+import TopPools from "./components/TopPools/TopPools";
 
-function App() {
+const { TabPane } = Tabs;
+
+const { Title } = Typography;
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Title>Uniswap Visualization</Title>
+        <Tabs onChange={() => console.log("clicked Tab")} type="card">
+          <TabPane tab="Top Pools" key="1">
+            <TopPools />
+          </TabPane>
+          <TabPane tab="Tokens" key="2">
+            Content of Tab Pane 2
+          </TabPane>
+          <TabPane tab="Transactions" key="3">
+            Content of Tab Pane 3
+          </TabPane>
+        </Tabs>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
